3cd34d8e192c329e69220c78745650bd63803368
vagrant <vagrant@homestead>
2018-06-04 16:30:45 +0000

    reply and notifications

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -14,6 +14,52 @@ export default class ReplyMixin extends wepy.mixin {
 		page: 1
 	}
 
+	methods = {
+		// 删除回复
+		async deleteReply(topicId, replyId) {
+			// 确认是否删除
+			let res = await  wepy.showModal({
+				title: '确认删除',
+				content: '您确认删除该回复吗',
+				confirmText: '删除',
+				cancelText: '取消'
+			})
+
+			// 点击取消后返回
+			if (!res.confirm) {
+				return
+			}
+			try {
+				// 调用接口删除回复
+				let deleteResponse = await api.authRequest({
+					url: 'topics/' + topicId + '/replies/' + replyId,
+					method: 'DELETE'
+				})
+
+				// 删除成功
+				if (deleteResponse.statusCode === 204) {
+					wepy.showToast({
+						title: '删除成功',
+						icon: 'success',
+						duration: 2000
+					})
+
+					// 将删除了的回复移除
+					this.replies = this.replies.filter((reply) => reply.id !== replyId)
+					this.$apply()
+				}
+
+				return deleteResponse
+			} catch (err) {
+				console.log(err)
+				wepy.showModal({
+					title: '提示',
+					content: '服务器错误，请联系管理员'
+				})
+			}
+		}
+	}
+
 	// 获取话题回复
 	async getReplies(reset = false) {
 		try {
@@ -29,8 +75,20 @@ export default class ReplyMixin extends wepy.mixin {
 			if (repliesResponse.statusCode === 200) {
 				let replies = repliesResponse.data.data
 
+				// 获取当前用户
+				let user = await this.$parent.getCurrentUser()
+
 				// 格式化回复创建时间
 				replies.forEach(function (reply) {
+					// 控制是否可以删除
+					if (!user) {
+						reply.can_delete = false
+					} else {
+						reply.can_delete = (reply.user_id === user.id)
+					}
+
+					// reply.can_delete = this.canDelete(user, reply)
+
 					reply.created_at_diff = util.diffForHumans(reply.created_at)
 				})
 				// 如果reset不为true则合并 this.replies；否则直接覆盖
@@ -54,6 +112,14 @@ export default class ReplyMixin extends wepy.mixin {
 			})
 		}
 	}
+
+	// canDelete(user, reply) {
+	// 	if (!user) {
+	// 		return false
+	// 	}
+	//
+	// 	return (reply.user_id === user.id)
+	// }
 
 	async onPullDownRefresh() {
 		this.noMoreData = false
